684795cbdebe7817e128e7392b3e433d10341ea6
Simo Kinnunen <simo@shoqolate.com>
2017-06-19 15:15:44 +0900

    Add eslint config and bulk-decaf options.

---
+++ bulk-decaffeinate.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  decaffeinateArgs: [
+    '--keep-commonjs',
+    '--prefer-const',
+    '--loose-default-params',
+    '--loose-for-expressions',
+    '--loose-for-of',
+    '--loose-includes'
+  ],
+  mochaEnvFilePattern: /test\//
+}
